af88eb71e9a3a49dccd03bfea9c2c39cf4d95601
alexanderpetelin <aepetelin@gmail.com>
2019-06-09 21:41:07 +0700

    MVVM
    https://www.youtube.com/watch?v=jlcmmWuZyvY

---
+++ WPFPatternMVVM/WPFPatternMVVM/Data/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WPFPatternMVVM.Data
+{
+    class Person
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public static Person[] GetPersons() 
+        {
+            var result = new Person[]
+            {
+                new Person{LastName="Иванов",FirstName="Иван"},
+                new Person{LastName="Петров",FirstName="Петр"}
+            };
+            return result;
+        }
+    }
+}
---
+++ WPFPatternMVVM/WPFPatternMVVM/ViewModel/PersonsViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using WPFPatternMVVM.Data;
+
+namespace WPFPatternMVVM.ViewModel
+{
+    class PersonsViewModel : DependencyObject
+    {
+        public string FilterText
+        {
+            get { return (string)GetValue(FilterTextProperty); }
+            set { SetValue(FilterTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FilterTextProperty =
+            DependencyProperty.Register("FilterText", typeof(string), typeof(PersonsViewModel), new PropertyMetadata("",FilterText_Changed));
+
+        private static void FilterText_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            PersonsViewModel current = d as PersonsViewModel;
+            if(current!=null)
+            {
+                current.Items.Filter = null;
+                current.Items.Filter = current.FilterPerson;
+            }
+        }
+
+        public ICollectionView Items
+        {
+            get { return (ICollectionView)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemsProperty =
+            DependencyProperty.Register("Items", typeof(ICollectionView), typeof(PersonsViewModel), new PropertyMetadata(null));
+
+        public PersonsViewModel()
+        {
+            Items = CollectionViewSource.GetDefaultView(Person.GetPersons());
+            Items.Filter = FilterPerson;
+        }
+
+        private bool FilterPerson(object obj)
+        {
+            bool result = true;
+            Person person = obj as Person;
+            if(!string.IsNullOrWhiteSpace(FilterText) && person != null && !person.LastName.Contains(FilterText) && !person.FirstName.Contains(FilterText))
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
